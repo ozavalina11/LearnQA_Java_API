264840295ed24d56456b35f681dcc1204a4febfc
ozavalina <zavalina.olga11@gmail.com>
2023-05-04 00:21:54 +0300

    added oneEx4()

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class Homework {
@@ -5,5 +7,13 @@ public class Homework {
     @Test
     public void testHello() {
         System.out.println("Hello from Olga");
+    }
+
+    @Test
+    public void oneEx4() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.print();
     }
 }
