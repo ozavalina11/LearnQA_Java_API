3f5630761c2218fd6c6847e1f41d184f06c1621a
ozavalina <zavalina.olga11@gmail.com>
2023-05-07 22:07:04 +0300

    added testEx8()

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -3,6 +3,11 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Thread.sleep;
+
 public class Homework {
 
     @Test
@@ -71,5 +76,54 @@ public class Homework {
             }
         }
         while (statusCode == 301);
+    }
+
+    @Test
+    public void testEx8() { //throws InterruptedException
+        // 1. Создается задача
+        String url = "https://playground.learnqa.ru/ajax/api/longtime_job";
+        JsonPath response = RestAssured
+                .get(url)
+                .jsonPath();
+
+        Integer seconds = response.get("seconds");
+        String token = response.get("token");
+        System.out.println("Задача создана с token: " + token + "\nЗадача будет готова через: " + seconds + "сек");
+
+        // 2. Выполняется один запрос с token ДО того, как задача готова, проверяется поле status
+        Map<String, String> params = new HashMap<>();
+        params.put("token", token);
+
+        response = RestAssured
+                .given()
+                .queryParams(params)
+                .get(url)
+                .jsonPath();
+        String error = response.get("error");
+        if (error == null) {
+            String status = response.get("status");
+            if (status.equals("Job is NOT ready")) {
+
+                // 3. Ожидание на seconds*1000 мс
+                try {
+                    sleep(seconds * 1000);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+                // 4. Выполнение запроса c token ПОСЛЕ того, как задача готова, проверка поля status и наличие поля result
+                response = RestAssured
+                        .given()
+                        .queryParams(params)
+                        .get(url)
+                        .jsonPath();
+                status = response.get("status");
+                String result = response.get("result");
+                if (result != null && status.equals("Job is ready")) {
+                    System.out.println(status);
+                    System.out.println("Результат = " + result);
+                } else System.out.println(status);
+            } else System.out.println(status);
+        } else System.out.println("No job linked to this token");
     }
 }
