166546cb783caf2171feb27a0c002a2731871dc9
ozavalina <zavalina.olga11@gmail.com>
2023-05-13 22:13:57 +0300

    added testEx13()

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -3,6 +3,8 @@ import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -193,5 +195,30 @@ public class Homework {
         Headers headers = response.getHeaders();
         assertTrue(headers.hasHeaderWithName("X-Secret-Homework-Header"), "Response doesn't have 'X-Secret-Homework-Header' header");
         assertEquals("Some secret value", response.getHeader("X-Secret-Homework-Header"), "Unexpected header");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, Unknown, Unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"
+    })
+    public void testEx13(String userAgent, String platform, String browser, String device) {
+        JsonPath response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String resPlatform = response.getString("platform");
+        String resBrowser = response.getString("browser");
+        String resDevice = response.getString("device");
+
+        System.out.println(response.getString("user_agent"));
+
+        assertEquals(platform, resPlatform, "Неправильный параметр platform, его значение: " + resPlatform);
+        assertEquals(browser, resBrowser, "Неправильный параметр browser, его значение: " + resBrowser);
+        assertEquals(device, resDevice, "Неправильный параметр device, его значение: " + resDevice);
     }
 }
